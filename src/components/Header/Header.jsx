d9058ae2a8067a8c3e7ecddc0a65cb811d8ffc22
Lara Linhares <lararlinhares@gmail.com>
2024-01-25 00:04:09 -0300

    improve(header): Mobile Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,76 @@
-import React from 'react';
-import "./Header.css";
-import Logo from "../../images/logo.png";
-import Search from "../../images/ic.search.png"
+import React, { useState, useEffect } from 'react';
+import './Header.css';
+import Logo from '../../images/logo.png';
 
-function Header(){
-    return (
-        <>
-        <div className="upper-header">
-            <img src={Logo} alt="Logo intibiome" className="logo"/>
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const desktopNavItems = (
+    <ul>
+      <li><a href="#">about us</a></li>
+      <li><a href="#">our products</a></li>
+      <li><a href="#">intimate health</a></li>
+      <li><a href="#">contact us</a></li>
+    </ul>
+  );
+
+  const mobileNavItems = (
+    <ul>
+      <li><a href="#">brand philosophy</a></li>
+      <li><a href="#">product technology</a></li>
+      <li><a href="#">all products</a></li>
+      <li><a href="#">intibiome wellness</a></li>
+      <li><a href="#">intibiome active</a></li>
+      <li><a href="#">intibiome agecare</a></li>
+      <li><a href="#">article 1</a></li>
+      <li><a href="#">article 2</a></li>
+      <li><a href="#">article 3</a></li>
+      <li><a href="#">faq</a></li>
+      <li><a href="#">contact us</a></li>
+    </ul>
+  );
+
+  return (
+    <header>
+      <div className="logo-container">
+        <div className="logo">
+          <img src={Logo} alt="Logo" />
         </div>
 
-        <header className="main-header">
-            <nav>
-                <ul>
-                <li><a href="#">about us</a></li>
-                <li><a href="#">our products</a></li>
-                <li><a href="#">intimate health</a></li>
-                <li><a href="#">contact</a></li>
-                </ul>
-            </nav>
-        </header>
-        </>
-    );
-}
+        <nav className={`nav-links ${menuOpen ? 'show' : ''}`}>
+          {windowWidth > 768 ? desktopNavItems : mobileNavItems}
+        </nav>
+      </div>
 
-export default Header
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+
+
+
+
